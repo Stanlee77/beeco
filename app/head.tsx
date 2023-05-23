629d8c9f3f8b8bbfb193db165d2f014d484d9105
Stanisław Jarocki <s.jarocki354@gmail.com>
2023-05-24 00:57:58 +0200

    WIP

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,11 @@ export default function Head() {
     <>
       <title>BeEco</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Useful tool that helps recycle better" />
+      <meta
+        name="description"
+        content="Useful tool that helps recycle better"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  );
 }
