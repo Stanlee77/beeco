30a8cf663473a7da83dbb6af8b9a6e7dd547a1a0
MrJustice26 <vladislav2074@gmail.com>
2023-05-23 13:52:44 +0200

    Fix this shit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true  
   },
   env: {
-    DB_URI: "mongodb+srv://vladislav2074:zTtwxtxheaAUCG20@cluster0.8phav4h.mongodb.net/products?retryWrites=true&w=majority"
+    MONGODB_URI: "mongodb+srv://vladislav2074:zTtwxtxheaAUCG20@cluster0.8phav4h.mongodb.net/products?retryWrites=true&w=majority"
 
   }
 }
