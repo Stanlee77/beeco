58e1c2e82c7965eb512e01ac0f6ff7e548933271
Władysław Kuczerenko <wlakuc@st.amu.edu.pl>
2023-05-23 12:15:50 +0200

    Integrate mongodb with beeco project Create simple get rest API

---
+++ app/api/products/route.ts
@@ -0,0 +1,16 @@
+import clientPromise from "@/lib/mongodb"
+
+export async function GET(request: Request){
+
+    try {
+        const client = await clientPromise;
+        const db = client.db("products")
+
+        const items = await db.collection("items").find({}).limit(20).toArray();
+
+        return new Response(JSON.stringify(items));
+    } catch(e: any) {
+        console.error(e);
+        throw new Error(e).message;
+    }
+}
--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    DB_URI: "mongodb+srv://vladislav2074:zTtwxtxheaAUCG20@cluster0.8phav4h.mongodb.net/products?retryWrites=true&w=majority"
+
+  }
 }
 
 module.exports = nextConfig
