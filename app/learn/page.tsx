fb1e4539b3c74eb85d7b190900dfd465696264a9
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2023-05-24 01:18:26 +0200

    add content to learn/

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -2,14 +2,21 @@ import LearnLink from "./components/LearnLink";
 
 const Page = () => {
   return (
-    <ul className="flex flex-row w-full justify-center align-center gap-5 fixed bottom-[100px]">
-      <li>
-        <LearnLink url="/learn/quiz" title="Quiz" />
-      </li>
-      <li>
-        <LearnLink url="/learn/tips" title="Ciekawostka" />
-      </li>
-    </ul>
+    <>
+    <h1 className="p-10 py-5 text-[35px] text-sans mx-1 flex text-center items-center">Baw się i ucz, beeco to klucz!</h1>
+    <div className="px-7 mt-5 text-[16px] text-sans mx-1 flex text-center items-center">To właśnie tutaj możesz poszerzać swoją wiedzę na temat segregacji! Wybierz przystępną formę nauki połączonej z rozrywką. Lubisz rywalizację? W takim razie zakładka Quiz jest dla Ciebie! Może wolisz przyswajać wiedzę poprzez ciekawostki? Koniecznie zajrzyj w zakładkę Ciekawostki!</div>
+    <img 
+      className="w-52 h-52 mx-auto mt-28"
+      src="/beeco.png"
+      alt="beeco" />
+      <ul className="flex flex-row w-full justify-center align-center gap-5 fixed bottom-[100px]">
+        <li>
+          <LearnLink url="/learn/quiz" title="Quiz" />
+        </li>
+        <li>
+          <LearnLink url="/learn/tips" title="Ciekawostka" />
+        </li>
+      </ul></>
   );
 };
 
