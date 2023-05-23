6705e655cb3ca41a5561fb6d4b9bfa6841dfbd7e
Stanisław Jarocki <s.jarocki354@gmail.com>
2023-05-23 12:35:33 +0200

    fix project run

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className="p-2 bg-black d">Hello World</div>
+    <main>
+      <div className="p-2 bg-black text-white">Hello World</div>
     </main>
   )
 }
